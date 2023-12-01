b224b50ebc54fe4a1b6a76841949776ee9c42dae
rayzen2002 <emanuelzsmj@gmail.com>
2023-11-30 21:07:24 -0300

    `In Construction` page creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { SideBarMenu } from "@/components/SideBarMenu";
 
 export default function Home() {
   return (
-   <>
-    <h1>SMIPE</h1>
-   </>
+   <div className="grid grid-cols-7">
+    <SideBarMenu />
+    <div className="col-span-4 flex flex-col justify-start px-4 ">
+      <h1 className=" text-xl">Sistema de Manutenção e Informatização de Processos Escolares</h1>
+      <p className="text-5xl mt-16 justify-center items-center">Em contrução...</p>
+    </div>
+   </div>
   )
 }
---
+++ src/components/SideBarMenu.tsx
@@ -0,0 +1,9 @@
+export function SideBarMenu(){
+  return (
+    <div className="bg-teal-800 col-span-1 h-screen rounded-tr-xl rounded-br-xl">
+      <p>Bem-vindo,</p>
+      <p>#########</p>
+      {/* <button>Profile View</button> */}
+    </div>
+  )
+}
